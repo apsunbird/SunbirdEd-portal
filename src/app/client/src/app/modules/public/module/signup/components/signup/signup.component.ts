942bd89afc5aeb78aa246bead79c37cba8367ed3
chakshugulati <chakshu.gulati@velocis.co.in>
2021-07-18 18:04:56 +0530

    changing sign up fle

--- src/app/client/src/app/modules/public/module/signup/components/signup/signup.component.ts
+++ src/app/client/src/app/modules/public/module/signup/components/signup/signup.component.ts
@@ -241,7 +241,7 @@ export class SignupComponent implements OnInit, OnDestroy, AfterViewInit {
       "KEY":'F5FC9F4A7EEDF37C93FFBDCFB34C5D1829984C5B3A6FDB3B95457CD324'
       }
   
-    this.http.post('https://apgsws.in/learner/user/v1/gethrmsData?HRMS_ID='+this.userHrmsIdForm.value['addHrmsId'], data).subscribe(res => {
+    this.http.post('http://localhost:3000/learner/user/v1/gethrmsData?HRMS_ID='+this.userHrmsIdForm.value['addHrmsId'], data).subscribe(res => {
       console.log(res);
      // this.hrmsData  = res;
      this.userRegForm = true;
@@ -259,7 +259,7 @@ export class SignupComponent implements OnInit, OnDestroy, AfterViewInit {
         "SECRETARIAT_CODE": res['result'][0]['SECRETARIAT_CODE'],
         "MANDAL_NAME": res['result'][0]['MANDAL_NAME'],
         "DISTRICT_NAME": res['result'][0]['DISTRICT_NAME'],
-        "HRMS_ID":res['result'][0]['DISTRICT_NAME'],
+        "HRMS_ID":res['result'][0]['HRMS_ID'],
         "CFMS_ID":res['result'][0]['CFMS_ID']
         
       }
@@ -272,113 +272,7 @@ export class SignupComponent implements OnInit, OnDestroy, AfterViewInit {
 
   }
 
-  hrmsIdSubmit22(){  
-    this.tenantService.getHrmsData(this.userHrmsIdForm.value['addHrmsId'])
-    .subscribe((res: Response) => {
-      console.log(res);
-      
-      this.hrmsData  = res;
-        this.userRegForm = true;
-        this.genericPoup = false;
-        this.hrmsIDPopup = false;
-        console.log('first========');
-        if(this.hrmsData['status']==true)
-        {
-        if(this.hrmsData.MOBILE!=null)
-        {
-          this.editMobile = true;
-        }
 
-        if(this.hrmsData.EMP_NAME!=null)
-        {
-          this.editName = true;
-        }
-
-        if(this.hrmsData.HRMS_ID!=null)
-        {
-          this.hrmsIdData = true;
-        }
-
-
-        if(this.hrmsData.GENDER!=null)
-        {
-          this.genderData = true;
-        }
-
-        if(this.hrmsData.CFMS_ID!=null)
-        {
-          this.cfmsID = true;
-        }
-
-        if(this.hrmsData.SECRETARIAT_NAME!=null)
-        {
-          this.secName = true;
-        }
-
-        if(this.hrmsData.SECRETARIAT_CODE!=null)
-        {
-          this.secCode = true;
-        }
-
-        if(this.hrmsData.MANDAL_NAME!=null)
-        {
-          this.mandalName = true;
-        }
-
-        
-
-
-        if(this.hrmsData.DISTRICT_NAME!=null)
-        {
-          this.districtName  = true;
-        }
-
-        if(this.hrmsData.QUALIFICATION!=null)
-        {
-          this.qualificationlist  = true;
-        }
-
-        if(this.hrmsData.DESIGNATION!=null)
-        {
-          this.designation  = true;
-        }
-
-
-       this.hrmsData= {
-          'MOBILE':res['result'][0]['MOBILE'],
-          'EMP_MAIL_ID':res['result'][0]['EMP_MAIL_ID'],
-          'EMP_NAME':res['result'][0]['EMP_NAME'],
-          'GENDER':res['result'][0]['GENDER'],
-          'QUALIFICATION':res['result'][0]['QUALIFICATION'],
-          'DESIGNATION':res['result'][0]['DESIGNATION'],
-          'DEPARTMENT':res['result'][0]['DEPARTMENT'],
-          "SECRETARIAT_NAME": res['result'][0]['SECRETARIAT_NAME'],
-          "SECRETARIAT_CODE": res['result'][0]['SECRETARIAT_CODE'],
-          "MANDAL_NAME": res['result'][0]['MANDAL_NAME'],
-          "DISTRICT_NAME": res['result'][0]['DISTRICT_NAME'],
-          "HRMS_ID":res['result'][0]['DISTRICT_NAME'],
-          "CFMS_ID":res['result'][0]['CFMS_ID']
-          
-        }
-     
-      }
-
-    else if(this.hrmsData['status']==false)
-      {
-        this.genericPoup = true;
-        this.hrmsIDPopup = false;
-        this.genericMsg ="Invalid HRMS ID";
-        console.log('second========');
-      }
-
-    });
-  
-
-
-
-    
-
-  }
 
 
 
